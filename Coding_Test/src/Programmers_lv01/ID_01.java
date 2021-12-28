85b7d41efa9a1a811d7083524848047c658384d4
admin <admin@YD5-21>
2021-12-28 17:33:39 +0900

    coding_test

---
+++ Coding_Test/src/Programmers_lv01/ID_01.java
@@ -0,0 +1,20 @@
+package Programmers_lv01;
+
+import java.util.Scanner;
+
+public class ID_01 {
+
+	public String solution(String new_id) {
+		
+		Scanner new_id1 = new Scanner(System.in);
+		String new_id0 = new_id1.nextLine();
+		
+		new_id0 = new_id0.substring(0,3);
+		new_id0 = new_id0.substring(16,999);
+		
+		
+		String answer = "";
+		return answer;
+	}
+
+}
---
+++ Coding_Test/src/Programmers_lv01/ID_02.java
@@ -0,0 +1,17 @@
+package Programmers_lv01;
+
+import java.util.Scanner;
+
+public class ID_02 {
+
+	public static void main(String[] args) {
+		Scanner new_id = new Scanner(System.in);
+		String new_id0 = new_id.nextLine();
+		
+		
+		
+		String answer = "";
+		return ;
+	}
+
+}
